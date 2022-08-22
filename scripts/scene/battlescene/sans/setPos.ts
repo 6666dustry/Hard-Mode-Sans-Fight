3112158d201a274d2a84ef12ab4a572ccdf4f04a
many bear <98581612+6666dustry@users.noreply.github.com>
2022-08-22 12:03:01 +0900

    add tween.

--- scripts/scene/battlescene/sans/setPos.ts
+++ scripts/scene/battlescene/sans/setPos.ts
@@ -11,17 +11,78 @@ export default function setSansPosition(this: Sans, config: AllReadonly<SetPosit
         y: {
             type: ["number", "string", "boolean"],
             default: false
+        },
+        ease: {
+            type: "string",
+            default: "Linear"
+        },
+        duration: {
+            type: ["number", "boolean"],
+            default: false
         }
     }, this.director.AttackLoader.runAttackPos);
-    if (DATA.x !== false) {
-        this.x = DATA.x;
-    }
-    if (DATA.y !== false) {
-        if (DATA.y === "auto" || DATA.y === true) {
-            this.yAuto = true;
-        } else {
+
+    if (DATA.duration) {
+        if (typeof DATA.x === "number" && typeof DATA.y === "number") {
             this.yAuto = false;
-            this.y = DATA.y || this.y;
+            this.scene.tweens.add(
+                {
+                    targets: this,
+                    props: {
+                        x: DATA.x,
+                        y: DATA.y
+                    },
+                    duration: DATA.duration,
+                    ease: DATA.ease
+                }
+            );
+        } else {
+            if (typeof DATA.x === "number") {
+                this.scene.tweens.add(
+                    {
+                        targets: this,
+                        props: {
+                            x: DATA.x,
+                        },
+                        duration: DATA.duration,
+                        ease: DATA.ease
+                    }
+                );
+            }
+            if (DATA.y !== false) {
+                if (DATA.y === "auto") {
+                    this.yAuto = true;
+                } else if (DATA.y === "lock") {
+                    this.yAuto = false;
+                } else {
+                    this.yAuto = false;
+                    this.scene.tweens.add(
+                        {
+                            targets: this,
+                            props: {
+                                y: DATA.y,
+                            },
+                            duration: DATA.duration,
+                            ease: DATA.ease
+                        }
+                    );
+                }
+            }
+        }
+
+    } else {
+        if (DATA.x !== false) {
+            this.x = DATA.x;
+        }
+        if (DATA.y !== false) {
+            if (DATA.y === "auto") {
+                this.yAuto = true;
+            } else if (DATA.y === "lock") {
+                this.yAuto = false;
+            } else {
+                this.yAuto = false;
+                this.y = DATA.y;
+            }
         }
     }
 }
